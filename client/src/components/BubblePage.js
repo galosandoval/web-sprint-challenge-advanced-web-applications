c3193ee74c929fe0049ca5e6de00cae3ac557049
Galo Sandoval <galosan92@gmail.com>
2020-08-21 09:53:47 -0700

    functionality is working!

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -24,7 +24,7 @@ const BubblePage = () => {
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList colors={colorList} updateColors={setColorList} fetchBubbles={fetchBubbles} />
      <Bubbles colors={colorList} />
     </>
   );

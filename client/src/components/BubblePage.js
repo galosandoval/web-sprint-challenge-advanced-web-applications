15982ca155cbfb379a356adbad6de054bcc12ef2
Galo Sandoval <galosan92@gmail.com>
2020-08-21 10:53:16 -0700

    back to last step

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
@@ -9,9 +11,24 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  const fetchBubbles = () => {
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => setColorList(res.data))
+      .catch((err) => err);
+  };
+
+  useEffect(() => {
+    fetchBubbles();
+  }, []);
+
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList
+        colors={colorList}
+        updateColors={setColorList}
+        fetchBubbles={fetchBubbles}
+      />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+
+const initialState = {
+  username: "",
+  password: "",
+};
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [creds, setCreds] = useState(initialState);
+  const history = useHistory();
+
+  const handleChange = (e) => {
+    setCreds({
+      ...creds,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("api/login", creds)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        history.push("/bubble-page");
+      })
+      .catch((err) => console.log(err, "lol"));
+    setCreds(creds);
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={login}>
+      <input
+        name="username"
+        type="text"
+        placeholder="Username"
+        value={creds.username}
+        onChange={handleChange}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={creds.password}
+        onChange={handleChange}
+      />
+      <button>Log In</button>
+    </form>
     </>
   );
 };
